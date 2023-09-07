5ba1d776d2e1e6cb013e8464cf8e8323d15ac944
Krzysztof S. Matejak <krzysztof@matejak.com>
2023-09-07 19:38:08 +0200

    add routing, routerLink -- pages not found, movies, categories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
-import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-import { MovieCoverComponent } from './shared/movie-cover/movie-cover.component';
-import { MovieDetailsComponent } from './pages/movies/movie-details/movie-details.component';
-import { MoviesComponent } from './pages/movies/movies.component';
-import { CategoriesComponent } from './pages/categories/categories.component';
-import { MoviesInCategoryComponent } from './pages/categories/movies-in-category/movies-in-category.component';
+import { PageNotFoundComponent } 
+    from './pages/page-not-found/page-not-found.component';
+import { MovieCoverComponent } 
+    from './shared/movie-cover/movie-cover.component';
+import { MovieDetailsComponent } 
+    from './pages/movies/movie-details/movie-details.component';
+import { MoviesComponent } 
+    from './pages/movies/movies.component';
+import { CategoriesComponent } 
+    from './pages/categories/categories.component';
+import { MoviesInCategoryComponent } 
+    from './pages/categories/movies-in-category/movies-in-category.component';
+import { AppRoutingModule } 
+    from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -21,7 +29,11 @@ import { MoviesInCategoryComponent } from './pages/categories/movies-in-category
     MoviesInCategoryComponent,
     MovieCoverComponent,
   ],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule, 
+    HttpClientModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/page-not-found/page-not-found.component.ts
+++ src/app/pages/page-not-found/page-not-found.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
-  template: '<h1>Page Not Found</h1>',
+  template: '<h1 class="text-align: center">Page Not Found</h1>',
 })
 export class PageNotFoundComponent implements OnInit {
   constructor() {}
