2d981f811307c73f77937a1b7886c1f32d2b32ec
Krzysztof S. Matejak <krzysztof@matejak.com>
2023-09-07 21:24:34 +0200

    add router param fo movie page, ActivatedRoute

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from "@angular/router";
 import { MoviesComponent } from "./pages/movies/movies.component";
 import { PageNotFoundComponent } from "./pages/page-not-found/page-not-found.component";
 import { CategoriesComponent } from "./pages/categories/categories.component";
+import { MovieDetailsComponent } from "./pages/movies/movie-details/movie-details.component";
 
 
 const routes: Routes = [
     { path: '', redirectTo: '/movies', pathMatch: 'full' },
     { path: 'movies', component: MoviesComponent },
     { path: 'categories', component: CategoriesComponent },
+    { path: 'movie/:id', component: MovieDetailsComponent },
 
     { path: '**', component: PageNotFoundComponent },
 ]
