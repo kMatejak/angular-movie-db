44e10702b1ab66a772811a257b38ac169d911421
Krzysztof S. Matejak <krzysztof@matejak.com>
2023-09-12 15:05:53 +0200

    add basic form validation

--- src/app/pages/add-movie/add-movie.component.ts
+++ src/app/pages/add-movie/add-movie.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Movie } from 'src/app/models/movie';
+import { HttpMoviesService } from 'src/app/services/http-movies.service';
 import { HttpService } from 'src/app/services/http.service';
 
 @Component({
@@ -13,18 +14,25 @@ export class AddMovieComponent implements OnInit {
   categories: string[] = [];
   years: string[] = [];
 
-  constructor(public http: HttpService) { }
+  constructor(
+    private http: HttpService,
+    private httpMoviesService: HttpMoviesService
+  ) { }
 
   ngOnInit(): void {
     this.http.getYears()
       .subscribe(years => this.years = years);
-      
+
     this.http.getCategories()
       .subscribe(categories => this.categories = categories);
   }
 
   send() {
     console.log(this.model);
+    this.httpMoviesService.postMovie(this.model as Movie).subscribe(
+      result => console.log(result),
+      error => console.log(error)
+    )
   }
 
 }
